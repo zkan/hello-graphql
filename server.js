f2f9cc3a78fbd8946ce309334e20de1a111d0308
zkan <kan@prontomarketing.com>
2017-08-22 14:17:47 +0700

    Implement mutation

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ var express = require('express')
 var graphqlHTTP = require('express-graphql')
 var { buildSchema } = require('graphql')
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
-
-  type Query {
-    getDie(numSides: Int): RandomDie
-  }
-`);
+}
 
 // This class implements the RandomDie GraphQL type
 class RandomDie {
@@ -34,6 +30,39 @@ class RandomDie {
   }
 }
 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Query {
+    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+`);
+
+// Maps username to content
+var fakeDatabase = {};
+
 // The root provides a resolver function for each API endpoint
 var root = {
   quoteOfTheDay: () => {
@@ -44,7 +73,28 @@ var root = {
   },
   getDie: function ({numSides}) {
     return new RandomDie(numSides || 6);
-  }
+  },
+  getMessage: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
 }
 
 var app = express()
