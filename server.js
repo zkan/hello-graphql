26c74ca4c15a93feb23797b2aaa7b4d3f4d06b60
zkan <kan@prontomarketing.com>
2017-08-22 14:01:10 +0700

    Create new object type

--- server.js
+++ server.js
@@ -1,38 +1,57 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql')
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `);
+
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides)
+  }
+
+  roll({numRolls}) {
+    var output = []
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce())
+    }
+    return output
+  }
+}
 
 // The root provides a resolver function for each API endpoint
 var root = {
   quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
   },
   random: () => {
-    return Math.random();
+    return Math.random()
   },
-  rollDice: ({numDice, numSides}) => {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
-  },
-};
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6);
+  }
+}
 
-var app = express();
+var app = express()
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+}))
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
